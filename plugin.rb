6fe15bf252b02f90a890449a14f0f2d019586628
frold <abb@liljefred.dk>
2023-05-07 16:40:09 +0200

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-image-bot
+# about: Replies a new topic with a images based on values added in the previous post
+# version: 0.1
+# authors: Frederik Liljefred
+# url: https://github.com/frold/discourse-image-bot
